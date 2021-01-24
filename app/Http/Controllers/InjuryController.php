b04cafff3cde6387f70687bd14a73e8ef508b403
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-25 00:44:02 +0900

    InjuryControllerの作成

---
+++ app/Http/Controllers/InjuryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InjuryController extends Controller
+{
+    public function index(){
+        $injuries = Injury::all();
+
+        return view('injury.index')
+    }
+}
