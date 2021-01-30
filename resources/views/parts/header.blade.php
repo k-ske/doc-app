3346ca52c9a25f1d96a19ebd4bf5ed466f21eb1a
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-31 00:43:20 +0900

    ヘッダー・フッター作成

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="css/parts/header.css">
+
+<div class="allHeader">
+  <div class="headArea">
+    <div class="headList">
+      <div class="headerLeft">
+        <div class="title"><a href="/">DOC-APP</a></div>
+      </div>
+      <div class="headerRight">
+        <div class="logIn"><a href="/">ログイン</a></div>
+        <div class="signUp"><a href="/">会員登録</a></div>
+      </div> 
+    </div>
+  </div>
+
+  <div class="subheadArea">
+    <div class="menuList">
+      <ul class="topMenu">
+        <li class="home">
+          <a href="/">ホーム</a>
+        </li>
+        <li class="articleSearch">
+          <a href="/">記事を検索する</a>
+        </li>
+        <li class="hospitalSearch">
+          <a href="/">病院を検索する</a>
+        </li>
+        <li class="subhead-logIn">
+          <a href="/">ログイン</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+    
