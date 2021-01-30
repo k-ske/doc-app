9e784c978fa50f58bb9780a7a283f6bd2f4b7c91
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-31 00:44:01 +0900

    スポーツトップページ作成

--- resources/views/sport/index.blade.php
+++ resources/views/sport/index.blade.php
@@ -1,10 +1,15 @@
-@foreach ($sports as $sport)
-  <p>小学生：{{ $sport->es_sport }}</p>
-  <p>詳細：{{ $sport->es_comment }}</p>
-  <p>中学生：{{ $sport->jhs_sport }}</p>
-  <p>詳細：{{ $sport->jhs_comment }}</p>
-  <p>高校生：{{ $sport->hs_sport }}</p>
-  <p>詳細：{{ $sport->hs_comment }}</p>
-  <p>大学生：{{ $sport->co_sport }}</p>
-  <p>詳細：{{ $sport->co_comment }}</p>
-@endforeach
+@extends('layouts.common')
+@section('title', 'ユーザートップページ')
+
+@section('content')
+  @foreach ($sports as $sport)
+    <p>小学生：{{ $sport->es_sport }}</p>
+    <p>詳細：{{ $sport->es_comment }}</p>
+    <p>中学生：{{ $sport->jhs_sport }}</p>
+    <p>詳細：{{ $sport->jhs_comment }}</p>
+    <p>高校生：{{ $sport->hs_sport }}</p>
+    <p>詳細：{{ $sport->hs_comment }}</p>
+    <p>大学生：{{ $sport->co_sport }}</p>
+    <p>詳細：{{ $sport->co_comment }}</p>
+  @endforeach
+@endsection
