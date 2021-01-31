62f88fa22b5a4cb3d45d302a1f6e41a8ea2f048d
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-31 14:20:30 +0900

    画面遷移のルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('top');
+    return view('user/index');
 });
 
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect('/user/index');
+});
+
+Route::get('/login', function(){
+    return view('login');
+});
+
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+*/
 
-Route::resource('/user', 'App\Http\Controllers\UserController');
-
-Route::resource('/sport', 'App\Http\Controllers\SportController');
-
-Route::resource('/injury', 'App\Http\Controllers\InjuryController');
-
+Route::group(['middleware' => ['auth']], function(){
+    Route::resource('/user', 'App\Http\Controllers\UserController', ['only' => ['create', 'show', 'update', 'destroy', 'edit']]);
+    Route::resource('/sport', 'App\Http\Controllers\SportController');
+    Route::resource('/injury', 'App\Http\Controllers\InjuryController');
+});
 require __DIR__.'/auth.php';
