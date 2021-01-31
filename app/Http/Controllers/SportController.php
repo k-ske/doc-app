294bc31f0338c14e63aaa90f6795c79f180587b5
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-31 14:28:29 +0900

    スポーツ情報保存実装

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -19,7 +19,16 @@ class SportController extends Controller
 
     public function store(Request $request){
         $sport = new Sport;
-        $sport->fill($request->all())->save();
+        $sport->es_sport = $request->es_sport;
+        $sport->es_comment = $request->es_comment;
+        $sport->jhs_sport = $request->jhs_sport;
+        $sport->jhs_comment = $request->jhs_comment;
+        $sport->hs_sport = $request->hs_sport;
+        $sport->hs_comment = $request->hs_comment;
+        $sport->co_sport = $request->co_sport;
+        $sport->co_comment = $request->co_comment;
+        $sport->user_id = $request->user()->id;
+        $sport->save();
         return redirect() -> route('sport.index');
     }
 
