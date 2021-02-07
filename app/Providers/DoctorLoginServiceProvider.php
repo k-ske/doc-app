ce2a810b882b76396875d4a2b5809b040454facd
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-02-07 23:45:43 +0900

    Create DoctorLoginServiceProvider.php

---
+++ app/Providers/DoctorLoginServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Controllers\Doctor\Auth\LoginController;
+use App\Responses\DoctorLoginResponses;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Support\Facades\Auth;
+use App\Actions\Doctor\AttemptToAuthenticate;
+use Illuminate\Support\ServiceProvider;
+
+class DoctorLoginServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app
+            ->when([LoginController::class, AttemptToAuthenticate::class])
+            ->needs(StatefulGuard::class)
+            ->give(function () {
+                return Auth::guard('doctor');
+            });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
