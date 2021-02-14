a419336e1d7d6b70b9224df99ff5e41a3c6db64e
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-02-15 01:07:53 +0900

    Indexページのcss修正

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -6,24 +6,24 @@
 
   <div class="main-wrap">
     <div class="main-page">
-      <div class="injury-file">
-        <h3 class="profile">記事一覧</h3>
+      <div class="article-list">
+        <h3 class="top-bar">記事一覧</h3>
         <table class="table">
           <thead>
           <tr>
-              <th>作成日時</th>
-              <th>件名</th>
-              <th>メッセージ</th>
-              <th></th>
+              <th class="bar">作成日時</th>
+              <th class="bar">タイトル</th>
+              <th class="bar">内容</th>
+              <th class="bar"></th>
           </tr>
           </thead>
           <tbody id="tbl">
           @foreach ($articles as $article)
             <tr>
-              <td>{{ $article->created_at->format('Y.m.d') }}</td>
-              <td>{{ $article->title }}</td>
-              <td>{!! nl2br(e(Str::limit($article->content, 10))) !!}</td>
-              <td><a href="{{ action('App\Http\Controllers\ArticleController@show') }}">READ MORE</a></td>
+              <td class="article">{{ $article->created_at->format('Y.m.d') }}</td>
+              <td class="article">{{ $article->title }}</td>
+              <td class="article">{!! nl2br(e(Str::limit($article->content, 10))) !!}</td>
+              <td class="article"><a href="{{ route('article.show', $article->id) }}">READ MORE</a></td>
           @endforeach
           </tbody>
         </table>
