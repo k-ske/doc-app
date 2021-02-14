db86bad57cc0c084eb818714608b647d68376d97
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-02-15 00:03:51 +0900

    カラム型の変更

--- database/migrations/2021_02_13_202309_create_articles_table.php
+++ database/migrations/2021_02_13_202309_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->foreignId('doctor_id')->constrained('doctors');
             $table->timestamps();
         });
