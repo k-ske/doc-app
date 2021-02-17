b0cb54d77c6a6adc81110189f4f995865854fa1d
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-02-17 22:55:09 +0900

    Create EvaluationController.php

---
+++ app/Http/Controllers/EvaluationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\EvaluationRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Evaluation;
+use App\Models\Injury;
+use App\Models\Doctor;
+
+
+class EvaluationController extends Controller
+{
+    public function create(){
+        $q = \Request::query();
+        return view('evaluation.create', [
+            'injury_id' => $q['injury_id']
+        ]);
+    }
+
+    public function store(EvaluationRequest $request){
+        $evaluation = new Evaluation;
+        $input = $request->only($evaluation->getFillable());
+        $evaluation = $evaluation->create($input);
+
+        
+        return redirect('/doctor/top');
+    }
+}
